90922002e610de02a811501aefd07856d8cbb879
haikallf <haikallzrd@gmail.com>
2021-11-23 00:04:57 +0700

    added routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Bahan from "./components/Bahan";
 import Header from "./components/Header";
 import Login from "./components/Login";
@@ -7,14 +8,19 @@ import Resep from "./components/Resep";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <div className="blank"></div>
-      {/* <Login /> */}
-      {/* <Request /> */}
-      {/* <Resep /> */}
-      <Bahan />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <div className="blank"></div>
+        <Switch>
+          <Route exact path="/" component={Request} />
+          <Route path="/login" component={Login} />
+          <Route path="/request" component={Request} />
+          <Route path="/resep" component={Resep} />
+          <Route path="/bahan" component={Bahan} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
