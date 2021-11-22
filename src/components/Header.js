6602d5377a41e310b3a27026aa421f78fdbfa6b9
haikallf <haikallzrd@gmail.com>
2021-11-23 00:10:31 +0700

    sync route with header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,29 @@
 import React from "react";
 import "./Header.css";
-import { Router, Route, Switch } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <div className="header">
       <div className="header__container">
         <div className="header__left">
-          <p>Dorayaki Factory</p>
+          <Link to="/" className="header__leftLink">
+            <p>Dorayaki Factory</p>
+          </Link>
         </div>
 
         <div className="header__right">
-          <p>Requests</p>
-          <p>Resep</p>
-          <p>Bahan</p>
+          <Link to="/request" className="header__rightLink">
+            <p>Requests</p>
+          </Link>
+
+          <Link to="/resep" className="header__rightLink">
+            <p>Resep</p>
+          </Link>
+
+          <Link to="/bahan" className="header__rightLink">
+            <p>Bahan</p>
+          </Link>
         </div>
       </div>
     </div>
