f7aa51cb72bdb62401deccb327554ecef9eef41b
haikallf <haikallzrd@gmail.com>
2021-11-23 15:58:43 +0700

    table for bahan page

--- src/components/Bahan.js
+++ src/components/Bahan.js
@@ -1,47 +1,122 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import "./Bahan.css";
 import { url } from "../globalconfig";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import TablePagination from "@material-ui/core/TablePagination";
+import Paper from "@material-ui/core/Paper";
+import { Button } from "@material-ui/core";
 
 function Bahan() {
   const [bahan, setBahan] = useState([]);
+  const [rows, setRows] = useState([]);
+  const history = useHistory();
 
   useEffect(() => {
     getBahan();
-  }, [bahan]);
+  }, [rows]);
 
   const getBahan = async () => {
     const response = await axios.get(url + "/bahan");
-    console.log(response.data);
     setBahan(response.data);
+    setRows(response.data);
   };
+
+  const goToEditPage = (id) => {
+    history.push(`/editbahan/${id}`);
+  };
+
+  const useStyles = makeStyles({
+    table: {
+      minWidth: 650,
+    },
+  });
+
+  const classes = useStyles();
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+  const emptyRows =
+    rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
 
   return (
     <div className="bahan">
-      <Link to="/addbahan">Tambah Bahan</Link>
-      <table>
-        <thead>
-          <tr>
-            <th>ID Item</th>
-            <th>Nama Bahan</th>
-            <th>Stok Bahan</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {bahan.map((item) => (
-            <tr>
-              <td>{item.idBahan}</td>
-              <td>{item.namaBahan}</td>
-              <td>{item.stokBahan}</td>
-              <td>
-                <Link to={`/editbahan/${item.idBahan}`}>Edit</Link>
-                {/* <button>Edit</button> */}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <Button
+        variant="contained"
+        style={{
+          backgroundColor: "rgb(0, 180, 255)",
+          marginLeft: "70px",
+          marginTop: "30px",
+        }}
+        onClick={() => history.push("/addbahan")}
+      >
+        Tambah Bahan
+      </Button>
+      <div className="bahan__form">
+        <TableContainer component={Paper}>
+          <Table className={classes.table} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell align="center">ID Item</TableCell>
+                <TableCell align="center">Nama Bahan</TableCell>
+                <TableCell align="center">Stok Bahan</TableCell>
+                <TableCell align="center">Action</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row, index) => (
+                  <TableRow key={row.name}>
+                    <TableCell align="center">{row.idBahan}</TableCell>
+                    <TableCell align="center">{row.namaBahan}</TableCell>
+                    <TableCell align="center">{row.stokBahan}</TableCell>
+                    <TableCell align="center">
+                      <Button
+                        variant="outlined"
+                        style={{
+                          borderColor: "rgb(10, 153, 6)",
+                          color: "rgb(10, 153, 6)",
+                        }}
+                        onClick={() => goToEditPage(row.idBahan)}
+                      >
+                        Edit
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              {emptyRows > 0 && (
+                <TableRow style={{ height: 53 * emptyRows }}>
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+          <TablePagination
+            rowsPerPageOptions={[5, 10, 25]}
+            component="div"
+            count={rows.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onChangePage={handleChangePage}
+            onChangeRowsPerPage={handleChangeRowsPerPage}
+          />
+        </TableContainer>
+      </div>
     </div>
   );
 }
